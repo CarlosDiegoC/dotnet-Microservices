bf8375782db5b407fb91acb4516e68bb51cfedc1
Arcenio Souza <arcenio_neto@icloud.com>
2022-09-22 14:59:53 -0300

    GSAPI02-Create ValueObjects

---
+++ GeekShopping/GeekShopping.CartAPI/Data/ValueObjects/ProductVO.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeekShopping.CartAPI.Data.ValueObjects
+{
+    [Table("product")]
+    public class ProductVO
+    {        
+        public long Id { get; set; }       
+        public string? Name { get; set; }        
+        public decimal Price { get; set; }        
+        public string? Description { get; set; }    
+        public string? CategoryName { get; set; }
+        public string? ImageURL { get; set; }
+    }
+}
