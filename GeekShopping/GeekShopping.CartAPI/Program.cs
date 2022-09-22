d9f768143795de56f6886b3e0e6aa82352bf5577
Arcenio Souza <arcenio_neto@icloud.com>
2022-09-22 15:42:03 -0300

    GSAPI02-Add scoped for CardRepository and configure server connection port

--- GeekShopping/GeekShopping.CartAPI/Program.cs
+++ GeekShopping/GeekShopping.CartAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GeekShopping.CartAPI.Config;
 using GeekShopping.CartAPI.Model.Context;
+using GeekShopping.CartAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -19,7 +20,7 @@ IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-//builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
 
 builder.Services.AddControllers();
 
