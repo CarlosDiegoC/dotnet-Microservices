6579b80b74c4675f9326d03b11f63a532733175f
Carlos Costa <carlos_diegoh@homail.com>
2022-09-26 14:46:06 -0300

    GSMVC - Api product frontend implemented.

---
+++ GeekShopping/GeekShopping.Web/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        public async Task<ActionResult> ProductsIndex()
+        {
+            var products = await _productService.FindAllProducts();
+            return View(products);
+        }
+        
+        public async Task<ActionResult> ProductsCreate()
+        {   
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ProductsCreate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(model);
+                if(response != null) return RedirectToAction(nameof(ProductsIndex));
+            }
+            
+            return View(model);
+        }
+
+        public async Task<ActionResult> ProductsUpdate(int id)
+        {
+            var model = await _productService.FindProductById(id);
+            if(model != null) return View(model);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ProductsUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+                if (response != null) return RedirectToAction(nameof(ProductsIndex));
+            }
+
+            return View(model);
+        }
+
+        public async Task<ActionResult> ProductsDelete(int id)
+        {
+            var model = await _productService.FindProductById(id);
+            if (model != null) return View(model);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ProductsDelete(ProductModel model)
+        {
+            var response = await _productService.DeleteProductById(model.Id);
+            if (response) return RedirectToAction(nameof(ProductsIndex));      
+            return View(model);
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.Web/Startup.cs
@@ -0,0 +1,44 @@
+﻿using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
+namespace GeekShopping.Web
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient<IProductService, ProductService>(c => c.BaseAddress = new Uri(Configuration["ServiceUrls:ProductAPI"]));
+            services.AddControllersWithViews();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline. //Configure()
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
